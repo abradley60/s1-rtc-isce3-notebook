c141b6f814822aed848f07eb957988532db12622
abradley60 <alexbradley60@gmail.com>
2024-05-31 14:38:29 +0930

    upgrading software versions

--- setup.sh
+++ setup.sh
@@ -6,11 +6,11 @@ set -e
 # installing OPERA s1-reader
 S1=./s1-reader
 
-curl -sSL https://github.com/opera-adt/s1-reader/archive/refs/tags/v0.1.6.tar.gz -o s1_reader_src.tar.gz
+curl -sSL https://github.com/opera-adt/s1-reader/archive/refs/tags/v0.2.4.tar.gz -o s1_reader_src.tar.gz
 tar -zxvf s1_reader_src.tar.gz
 
 if ! [ -d "$S1" ]; then
-    ln -s s1-reader-0.1.6 "$S1"
+    ln -s s1-reader-0.2.4 "$S1"
 fi
 
 rm s1_reader_src.tar.gz
@@ -23,5 +23,12 @@ if [ -d "$RTC" ]; then
     rm -rf "$RTC"
 fi
 
-git clone https://github.com/opera-adt/RTC.git "$RTC"
+curl -sSL https://github.com/opera-adt/RTC/archive/refs/tags/v1.0.2.zip -o RTC.tar.gz
+tar -zxvf RTC.tar.gz
+
+if ! [ -d "$S1" ]; then
+    ln -s RTC-1.0.2 "$S1"
+fi
+
+rm RTC.tar.gz
 conda run -n isce3_rtc python -m pip install "$RTC"
